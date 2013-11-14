1f9a5194afc8c9901a188d08c0ed8e4e03b03277
niwatti <niwatti222withs@gmail.com>
2013-11-14 19:01:19 +0900

    (in progress) defining gluster_handler interfaces

---
+++ src/gluster_handler.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "api/glfs.h"
+#include "api/glfs-handles.h"
+#include <string.h>
+
+#include "gluster_handler.h"
+
---
+++ src/gluster_handler.h
@@ -0,0 +1,34 @@
+
+int
+gluster_handler_create();
+
+/* File Operations */
+int
+gluster_handler_open();
+
+int
+gluster_handler_create();
+
+/* Dir Operations */
+int
+gluster_handler_opendir();
+
+int
+gluster_handler_readdir_r();
+
+int
+gluster_handler_mkdir();
+
+/* Attribute Operations */
+int
+gluster_handler_getattr();
+
+int
+gluster_handler_setattr();
+
+/* XAttribute Operations */
+int
+gluster_handler_setxattr();
+
+int
+gluster_handler_listxattr();
